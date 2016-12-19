46a35f683dbefba9daf81d1f9da504f60ebf9cc1
Ian Ooi <ianooism@gmail.com>
2016-12-19 08:00:05 +0000

    customize generator

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Helpdesk
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+    g.test_framework = nil
+    g.assets = false
+    g.helper = false
+    g.jbuilder = false
+    end
   end
 end
