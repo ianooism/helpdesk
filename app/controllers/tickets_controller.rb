8acda6a9115bfe16ad6643dd50942303914ffdea
ianooism <ianooism@gmail.com>
2016-12-19 20:39:03 +0800

    Add ticket (#5)
    * associate project model to tickets
    
    * scaffold ticket
    
    * set route for ticket as nested resource
    
    * clean ticket controller
    
    * clean project controller
    
    * list tickets for project
    
    * create ticket for project
    
    * edit ticket for project
    
    * delete ticket for project

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,50 @@
+class TicketsController < ApplicationController
+  before_action :set_project
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @ticket = @project.tickets.build
+  end
+
+  def edit
+  end
+
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+
+    if @ticket.save
+      redirect_to project_ticket_url(@project, @ticket), notice: 'Ticket was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      redirect_to project_ticket_url(@project, @ticket), notice: 'Ticket was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ticket.destroy
+    redirect_to project_url(@project), notice: 'Ticket was successfully destroyed.'
+  end
+
+  private
+    def set_project
+      @project = Project.find(params[:project_id])
+    end
+    
+    def set_ticket
+      @ticket = @project.tickets.find(params[:id])
+    end
+
+    def ticket_params
+      params.require(:ticket).permit(:name, :description)
+    end
+end
